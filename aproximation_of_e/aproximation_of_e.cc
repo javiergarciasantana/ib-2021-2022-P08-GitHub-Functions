1cf83a81264312b20a54d16d6617b0d51ce1853d
usuario <javier.santana.12@ull.edu.es>
2021-11-24 19:22:48 +0000

    actualizar 4

---
+++ aproximation_of_e/aproximation_of_e.cc
@@ -0,0 +1,45 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Javier Garcia Santana || alu0101391663@ull.edu.es
+ * @date Nov 19 2021
+ * @brief The program prints the taylor series of the function e^x of a given number
+ * @see https://github.com/ULL-ESIT-IB-2021-2022/ib-2021-2022-P08-GitHub-Functions/blob/main/Functions-GitHub.md
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double Factorial (const double kInput) {
+  double factorial = 1.0;
+  for (double i = 1; i <= kInput; ++i) {
+    factorial *= i; 
+  }
+  return factorial;
+}
+
+double Aproximation_loop (const int kInput) {
+  int input = kInput;
+  double result = 0;
+  for (double i = 0; i < kInput; ++i) {
+    result += 1 / Factorial(i);
+  }
+  return result;
+}
+
+int main() {
+  int input;
+  while(cin >> input) {
+    if (input >= 0 && input <= 20) {
+      cout << "With " << input << " term(s) we get " << fixed << setprecision(10) << Aproximation_loop(input) << "." << endl;
+    } else {
+      cerr << "End-of-file reached or I/O error" << endl;
+    }
+  }
+  return 0;
+}
--- looking_for_an_a/looking_for_an_a.cc
+++ looking_for_an_a/looking_for_an_a.cc
@@ -7,7 +7,7 @@
  * @author Javier Garcia Santana || alu0101391663@ull.edu.es
  * @date Nov 19 2021
  * @brief The program prints yes if it finds an 'a' in the inserted sentence and otherwise it prints no
- * @see https://github.com/ULL-ESIT-IB-2021-2022/ib-2021-2022-P06-Statements/blob/master/Statements.md
+ * @see https://github.com/ULL-ESIT-IB-2021-2022/ib-2021-2022-P08-GitHub-Functions/blob/main/Functions-GitHub.md
  */
 
 #include <iostream>
@@ -16,7 +16,10 @@
 
 using namespace std;
 
-bool AFinder(const string kInput) {
+/*
+This boolean function determines whether the character 'a' is present on a given string
+*/
+bool AFinder(const string &kInput) {
   bool result = false;
   for (int i = 0; i < kInput.size(); ++i) {
     if (kInput[i] == 'a') {
@@ -26,21 +29,15 @@ bool AFinder(const string kInput) {
   return result;
 }
 
-void PrintResult(bool kResult) {
-  if (kResult == true) {
-    cout << "yes" << endl;
-  } else {
-    cout << "no" << endl;
-  }
-}
-
 int main() {
   string input;
-  getline(cin,input);
-  int size = input.size() - 1;
-  if (input[size] == '.'){
-    int result = AFinder(input);
-    PrintResult(result);
+  getline(cin, input);
+  if (input[input.size() - 1] == '.'){
+    if (AFinder(input)) {
+      cout << "yes" << endl;
+    } else {
+      cout << "no" << endl;
+    }
   } else {
     cerr << "End-of-file reached or I/O error" << endl;
   }
