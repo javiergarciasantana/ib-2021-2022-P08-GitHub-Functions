b6861aff61bebe4050cc48f4ae2b931b3b5271c3
usuario <javier.santana.12@ull.edu.es>
2021-11-25 18:01:19 +0000

    actualizar 11

--- P08_problemDescription/palindrome/palindrome.cc
+++ P08_problemDescription/palindrome/palindrome.cc
@@ -6,7 +6,17 @@ using namespace std;
 bool IsPalindrome(const string& kInput){
   int counter = 0;
   int size = kInput.size() - 1;
-  for (int i = kInput.size() - 1; i >= 0; --i) {
+  if (kInput.size() >= 50) {
+    for (int i = size; i = size - 3; --i) {
+      if (kInput[i] == kInput[size - i]) {
+        ++counter;
+      }
+    }
+  }
+  if (counter == 3) {
+    return true;
+  }
+  for (int i = size; i >= 0; --i) {
     if (kInput[i] == kInput[size - i]) {
         ++counter;
     }
@@ -20,10 +30,10 @@ bool IsPalindrome(const string& kInput){
 int main() {
   string input;
   getline(cin, input);
-  if(IsPalindrome(input)) {
+  if (IsPalindrome(input)) {
     cout << input << " is a palindrome" << endl;
   } else {
     cout << input << " is not a palindrome" << endl;
   }
-return 0;
+  return 0;
 }
